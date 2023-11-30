c58d9b147df7ddcf24cb7dbc374dafe174133e07
kirubaja <152260859+kirubaja@users.noreply.github.com>
2023-11-30 13:57:31 +0530

    Add files via upload

---
+++ reversearray.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$arr = [18, 25, 46, 33, 88, 99];
+echo "Elements in the array:";
+
+$arrReversed = array_reverse($arr);
+
+foreach ($arrReversed as $element) {
+    echo $element ."," ;
+}
+
+?>
+
+</body>
+</html>
+ 
+
+
